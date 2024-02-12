c04a40cb8fca9ad077264dd81175ca40034a81a8
Nataliya Kim <magistr1982@gmail.com>
2024-02-12 15:26:40 +0200

    Add graphql mutations and queries

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,6 +1,12 @@
-import { Button, Popover } from "antd"
+import { Popover } from "antd"
+import CustomAvatar from "../custom-avatar"
+import { useGetIdentity } from "@refinedev/core"
+
+import type {User} from "@/graphql/schema.types"
 
 const CurrentUser = () => {
+
+    const {data: user} = useGetIdentity<User>();
   return (
     <>
         <Popover
@@ -9,7 +15,7 @@ const CurrentUser = () => {
             overlayInnerStyle={{padding: 0}}
             overlayStyle={{zIndex: 999}}
     >
-        Click 
+       <CustomAvatar name=""/>
         {/* <Button></Button> */}
         </Popover>
     </>
