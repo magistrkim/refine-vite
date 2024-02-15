57090a1a925635327b05c584c1684ec082b7e25f
Nataliya Kim <magistr1982@gmail.com>
2024-02-15 09:32:40 +0200

    Add resources to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { App as AntdApp } from 'antd';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { Home, ForgotPassword, Login, Register } from './pages';
 import Layout from './components/layout';
+import { resources } from './config/resources';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
               liveProvider={liveProvider}
               notificationProvider={useNotificationProvider}
               authProvider={authProvider}
+              resources={resources}
               routerProvider={routerBindings}
               options={{
                 syncWithLocation: true,
