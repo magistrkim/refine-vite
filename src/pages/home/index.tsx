41fb8a0f556d119c40234d6ba772860218bdb183
Nataliya Kim <magistr1982@gmail.com>
2024-02-15 09:43:54 +0200

    Create Home page components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,37 @@
+import { Col, Row } from 'antd';
+import DealsChart from '@/components/home/DealsChart';
+import UpcomingEvents from '@/components/home/upcomingEvents';
+
 export const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px',
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          lg={8}
+          style={{
+            height: '460px',
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          lg={8}
+          style={{
+            height: '460px',
+          }}
+        >
+          <DealsChart />
+        </Col>
+      </Row>
+    </div>
+  );
 };
