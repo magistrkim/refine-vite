08efa80156486745240cb74866191fbae8388e3b
Nataliya Kim <magistr1982@gmail.com>
2024-02-15 10:21:01 +0200

    Add skeletons and UpcomingEvents

--- src/components/home/UpcomingEvents.tsx
+++ src/components/home/UpcomingEvents.tsx
@@ -1,9 +1,44 @@
-
+import { useState } from 'react';
+import { CalendarOutlined } from '@ant-design/icons';
+import { Card, List } from 'antd';
+import { Text } from '../text';
+import UpcomingEventsSkeleton from '../skeleton/upcoming-events';
 
 const UpcomingEvents = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <div>UpcomingEvents</div>
-  )
-}
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '0 1rem' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: '0.7rem' }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <List></List>
+      )}
+    </Card>
+  );
+};
 
-export default UpcomingEvents
+export default UpcomingEvents;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,17 @@
-import UpcomingEvents from "./home/upcomingEvents";
-import DealsChart from "./home/DealsChart";
+import UpcomingEvents from './home/UpcomingEvents';
+import DealsChart from './home/DealsChart';
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import LatestActivitiesSkeleton from './skeleton/latest-activities';
+import ProjectCardSkeleton from './skeleton/project-card';
 
-export {UpcomingEvents, DealsChart}
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+};
