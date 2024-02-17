ec0cfd4447715d8dcdc7dd74700015e55d4490c9
Nataliya Kim <magistr1982@gmail.com>
2024-02-17 09:57:43 +0200

    Add charts package to project

--- src/components/home/DealsChart.tsx
+++ src/components/home/DealsChart.tsx
@@ -1,5 +1,33 @@
+import { DollarOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+import { Text } from '../text';
+import { Area } from '@ant-design/plots';
+
 const DealsChart = () => {
-  return <div>DealsChart</div>;
+  const config = {};
+  return (
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '24px 24px 0 24px' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: '0.7rem' }}>
+            Deals Chart
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
 };
 
 export default DealsChart;
