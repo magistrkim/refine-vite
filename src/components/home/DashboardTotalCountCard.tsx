d47710fecd37d1391912a66fc2ef2dc1fcfb9fa4
Nataliya Kim <magistr1982@gmail.com>
2024-02-18 10:02:39 +0200

    Add constants to app

--- src/components/home/DashboardTotalCountCard.tsx
+++ src/components/home/DashboardTotalCountCard.tsx
@@ -1,9 +1,32 @@
+import { Card } from 'antd';
 
+type Props = {
+  resource: 'companies' | 'contacts' | 'deals';
+  isLoading: boolean;
+  totalCount: number;
+};
 
-const DashboardTotalCountCard = () => {
+const DashboardTotalCountCard = ({
+  resource,
+  isLoading,
+  totalCount,
+}: Props) => {
   return (
-    <div>DashboardTotalCountCard</div>
-  )
-}
+    <Card
+      style={{ height: '96px', padding: '0' }}
+      bodyStyle={{ padding: '8px 8px 8px 12px' }}
+      size="small"
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px',
+          whiteSpace: 'nowrap',
+        }}
+      ></div>
+    </Card>
+  );
+};
 
-export default DashboardTotalCountCard
+export default DashboardTotalCountCard;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,19 +4,41 @@ import {
   DealsChart,
   DashboardTotalCountCard,
 } from '@/components';
+import { useCustom } from '@refinedev/core';
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from '@/graphql/queries';
+import { DashboardTotalCountsQuery } from '@/graphql/types';
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: "",
+    method: "get",
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY
+    }
+  })
   return (
     <div>
       <Row gutter={[32, 32]}>
         <Col xs={24} sm={24} xl={8}>
-          <DashboardTotalCountCard />
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-          <DashboardTotalCountCard />
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-          <DashboardTotalCountCard />
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
         </Col>
       </Row>
       <Row
