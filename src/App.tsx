ff52084e12921704c7228ff44e5f0ecd8a027c11
Nataliya Kim <magistr1982@gmail.com>
2024-02-19 13:42:14 +0200

    Create CompaniesList page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import routerBindings, {
 } from '@refinedev/react-router-v6';
 import { App as AntdApp } from 'antd';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { Home, ForgotPassword, Login, Register } from './pages';
+import { Home, ForgotPassword, Login, Register, CompaniesList } from './pages';
 import Layout from './components/layout';
 import { resources } from './config/resources';
 
@@ -60,6 +60,7 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+                  <Route path="/companies" element={<CompaniesList />} />
                 </Route>
               </Routes>
               <RefineKbar />
---
+++ src/pages/companiesList/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const CompaniesList = () => {
+  return (
+    <div>CompaniesList</div>
+  )
+}
