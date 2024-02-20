187c1ff9aa7cda9baf154a9300cff06400a1a671
Nataliya Kim <magistr1982@gmail.com>
2024-02-20 15:23:49 +0200

    Add companies and amount price

--- src/pages/companies/list.tsx
+++ src/pages/companies/list.tsx
@@ -1,8 +1,25 @@
-import { CreateButton, List } from '@refinedev/antd';
-import { useGo } from '@refinedev/core';
+import { COMPANIES_LIST_QUERY } from '@/graphql/queries';
+import { CreateButton, FilterDropdown, List } from '@refinedev/antd';
+import { getDefaultFilter, useGo } from '@refinedev/core';
+import { useTable } from '@refinedev/antd';
+import { Table, Input, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import CustomAvatar from '@/components/custom-avatar';
+import { Text } from '@/components/text';
+import { Company } from '@/graphql/schema.types';
+import { currencyNumber } from '@/utilities';
 
 export const CompaniesList = () => {
   const go = useGo();
+  const { tableProps, filters } = useTable({
+    resource: 'companies',
+    pagination: {
+      pageSize: 12,
+    },
+    meta: {
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
   return (
     <List
       breadcrumb={false}
@@ -22,6 +39,39 @@ export const CompaniesList = () => {
           }}
         />
       )}
-    ></List>
+    >
+      <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
+        <Table.Column<Company>
+          title="Company Title"
+          dataIndex="name"
+          defaultFilteredValue={getDefaultFilter('id', filters)}
+          filterIcon={<SearchOutlined />}
+          filterDropdown={props => (
+            <FilterDropdown {...props}>
+              <Input placeholder="Search Company" />
+            </FilterDropdown>
+          )}
+          render={(value, record) => (
+            <Space>
+              <CustomAvatar
+                shape="square"
+                name={record.name}
+                src={record.avatarUrl}
+              />
+              <Text style={{ whiteSpace: 'nowrap' }}>{record.name}</Text>
+            </Space>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="totalRevenue"
+          title="Open deals amount"
+          render={(value, company) => (
+            <Text style={{ whiteSpace: 'nowrap' }}>
+              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+            </Text>
+          )}
+        />
+      </Table>
+    </List>
   );
 };
