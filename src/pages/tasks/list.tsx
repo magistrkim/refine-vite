8a18d5b4b37cce1d1e220f4b9e05d5245b5ed593
Nataliya Kim <magistr1982@gmail.com>
2024-03-01 09:20:33 +0200

    Add items to tasksList

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,20 +1,88 @@
+import React from 'react';
 import {
   KanbanBoardContainer,
   KanbanBoard,
 } from '@/components/tasks/kanban/board';
 import KanbanColumn from '@/components/tasks/kanban/column';
 import KanbanItem from '@/components/tasks/kanban/item';
+import { TASKS_QUERY, TASK_STAGES_QUERY } from '@/graphql/queries';
+import { useList } from '@refinedev/core';
+import { TaskStage } from '@/graphql/schema.types';
 
 export const TasksList = () => {
+  const { data: stages, isLoading: isLoadingStages } = useList({
+    resource: 'taskStages',
+    sorters: [
+      {
+        field: 'createdAt',
+        order: 'asc',
+      },
+    ],
+    filters: [
+      {
+        field: 'title',
+        operator: 'in',
+        value: ['TODO', 'IN PROGRESS', 'IN REVIEW', 'DONE'],
+      },
+    ],
+    meta: {
+      gqlQuery: TASK_STAGES_QUERY,
+    },
+  });
+
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+    resource: 'tasks',
+    sorters: [
+      {
+        field: 'dueDate',
+        order: 'asc',
+      },
+    ],
+    pagination: {
+      mode: 'off',
+    },
+    queryOptions: {
+      enabled: !!stages,
+    },
+    meta: {
+      gqlQuery: TASKS_QUERY,
+    },
+  });
+  const taskStages = React.useMemo(() => {
+    if (!tasks?.data || !stages?.data) {
+      return {
+        unassignedStage: [],
+        stages: [],
+      };
+    }
+
+    const unassignedStage = tasks.data.filter(task => task.stageId === null);
+
+    const grouped: TaskStage[] = stages.data.map(stage => ({
+      ...stage,
+      tasks: tasks.data.filter(task => task.stageId?.toString() === stage.id),
+    }));
+
+    return {
+      unassignedStage,
+      columns: grouped,
+    };
+  }, [stages, tasks]);
+  const handleAddCard = (args: { stageId: string }) => {};
   return (
     <div>
       <KanbanBoardContainer>
         <KanbanBoard>
-          <KanbanColumn>
-            <KanbanItem></KanbanItem>
+          <KanbanColumn
+            id="unassigned"
+            title={'unassigned'}
+            count={taskStages.unassignedStage.length || 0}
+            onAddClick={() => handleAddCard({ stageId: 'unassigned' })}
+          >
+            <KanbanItem>First ToDo</KanbanItem>
           </KanbanColumn>
           <KanbanColumn>
-            <KanbanItem></KanbanItem>
+            <KanbanItem>Second ToDo</KanbanItem>
           </KanbanColumn>
         </KanbanBoard>
       </KanbanBoardContainer>
