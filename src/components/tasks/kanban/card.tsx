01ec86d3bd44065fac4561472659e3a3ff614f45
Nataliya Kim <magistr1982@gmail.com>
2024-03-04 09:54:30 +0200

    Add user avatar to ProjectCard

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -1,3 +1,4 @@
+import CustomAvatar from '@/components/custom-avatar';
 import { Text } from '@/components/text';
 import { TextIcon } from '@/components/text-icon';
 import { User } from '@/graphql/schema.types';
@@ -14,7 +15,9 @@ import {
   ConfigProvider,
   Dropdown,
   MenuProps,
+  Space,
   Tag,
+  Tooltip,
   theme,
 } from 'antd';
 import dayjs from 'dayjs';
@@ -131,6 +134,26 @@ const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
             >
               {dueDateOptions.text}
             </Tag>
+          )}
+          {!!users?.length && (
+            <Space
+              size={4}
+              wrap
+              direction="horizontal"
+              align="center"
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginLeft: 'auto',
+                marginRight: 0,
+              }}
+            >
+              {users.map(user => (
+                <Tooltip key={user.id} title={user.name}>
+                  <CustomAvatar name={user.name} src={user.avatarUrl} />
+                </Tooltip>
+              ))}
+            </Space>
           )}
         </div>
       </Card>
